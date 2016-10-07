29cbb7187a8d336653d6402881a6d014c419bb0c
Kyle Geske <stungeye@gmail.com>
2016-10-07 12:19:52 -0500

    Added the product model and the associated controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  get 'products/index'
+
+  get 'products/show'
+
+  get 'products/new'
+
+  get 'products/create'
+
+  get 'products/edit'
+
+  get 'products/update'
+
+  get 'products/delete'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
