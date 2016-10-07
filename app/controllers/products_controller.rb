08a5a2d9174785bc7e811952d40f5854b7ed8517
Kyle Geske <stungeye@gmail.com>
2016-10-07 12:58:44 -0500

    Implemented the products index and show actions/views.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,14 @@
 class ProductsController < ApplicationController
   def index
+    # Instance variables are automagically shared with the view.
+    @products = Product.all
   end
+  # The associated index action will automagically be loaded.
 
   def show
+    @product = Product.find(params[:id])
   end
+  # automagically load show template with @product available
 
   def new
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,4 @@
 Rails.application.routes.draw do
-  get 'products/index'
-
-  get 'products/show'
-
-  get 'products/new'
-
-  get 'products/create'
-
-  get 'products/edit'
-
-  get 'products/update'
-
-  get 'products/delete'
-
+  resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
